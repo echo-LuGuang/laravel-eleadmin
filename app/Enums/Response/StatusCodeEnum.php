9a4baf2882efd01743000b6e45992d78b6bac1f5
WHOAMI <1720182606@qq.com>
2024-09-29 08:32:06 +0800

    feat: 添加pint.json配置，修改响应状态 一目了然

--- app/Enums/Response/StatusCodeEnum.php
+++ app/Enums/Response/StatusCodeEnum.php
@@ -9,35 +9,35 @@ enum StatusCodeEnum: int
     /**
      * 请求成功
      */
-    case HTTP_OK = Response::HTTP_OK;
+    case HTTP_OK = 200;
 
     /**
      * 请求失败
      */
-    case HTTP_BAD_REQUEST = Response::HTTP_BAD_REQUEST;
+    case HTTP_BAD_REQUEST = 400;
 
     /**
      * 未登录
      */
-    case HTTP_UNAUTHORIZED = Response::HTTP_UNAUTHORIZED;
+    case HTTP_UNAUTHORIZED = 401;
 
     /**
      * 请求内容不存在
      */
-    case HTTP_NOT_FOUND = Response::HTTP_NOT_FOUND;
+    case HTTP_NOT_FOUND = 404;
 
     /**
      * 请求方式不允许
      */
-    case HTTP_METHOD_NOT_ALLOWED = Response::HTTP_METHOD_NOT_ALLOWED;
+    case HTTP_METHOD_NOT_ALLOWED = 405;
 
     /**
      * 请求频繁
      */
-    case HTTP_TOO_MANY_REQUESTS = Response::HTTP_TOO_MANY_REQUESTS;
+    case HTTP_TOO_MANY_REQUESTS = 429;
 
     /**
      * 内部错误
      */
-    case HTTP_INTERNAL_SERVER_ERROR = Response::HTTP_INTERNAL_SERVER_ERROR;
+    case HTTP_INTERNAL_SERVER_ERROR = 500;
 }
